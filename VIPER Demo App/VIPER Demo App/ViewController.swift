4553aac3db6ab3663f42c5fa10febd6d2ee81af2
Anthony <Anthony.Gonzalez5195@gmail.com>
2020-10-14 17:49:48 -0400

    Project setup

---
+++ VIPER Demo App/VIPER Demo App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VIPER Demo App
+//
+//  Created by Anthony R. Gonzalez on 10/14/20.
+//  Copyright © 2020 Anthony R. Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .orange
+    }
+
+
+}
+
